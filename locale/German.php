07def81e4ffa8788200f2443f335688d410e5d17
Dennis Vorpahl <Webmeteor@users.noreply.github.com>
2017-03-01 14:10:31 +0100

    Create German.php

---
+++ locale/German.php
@@ -0,0 +1,28 @@
+<?php
+//allgemein
+$locale['WMKP001'] = 'Terminkalender';
+$locale['WMKP002'] = 'Ein Kalender zur Terminverwaltung';
+$locale['WMKP003'] = 'Markttermine';
+//kalender.php  admin.php
+$locale['WMKP004'] = 'Es sind noch keine Einträge im Kalender vorhanden';
+//DB einträge
+$locale['WMKP005'] = 'Der Termin wurde erfolgreich gelöscht';
+$locale['WMKP006'] = 'Der Termin konnte nicht gelöscht werden';
+$locale['WMKP007'] = 'Der Termin wurde erfolgreich eingetragen';
+$locale['WMKP008'] = 'Der Termin konnte nicht eingetragen werden';
+$locale['WMKP009'] = 'Der Termin wurde erfolgreich geändert';
+$locale['WMKP010'] = 'Der Termin konnte nicht geändert werden';
+$locale['WMKP011'] = 'Der Termin konnte nicht geändert werden';
+//admin form
+$locale['WMKP012'] = 'Name der Veranstaltung';
+$locale['WMKP013'] = 'Bitte trage den Namen der Veranstaltung ein';
+$locale['WMKP014'] = 'Beschreibung der Veranstaltung';
+$locale['WMKP015'] = 'Veranstaltungsanfang';
+$locale['WMKP016'] = 'Bitte trage das Datum des Anfangs der Veranstaltung ein';
+$locale['WMKP015a'] = 'Veranstaltungsende';
+$locale['WMKP016a'] = 'Bitte trage das Datum des Endes der Veranstaltung ein';
+$locale['WMKP017'] = 'speichern';
+$locale['WMKP018'] = 'Termin bearbeiten';
+$locale['WMKP019'] = 'Termin eintragen';
+//admin übersicht
+$locale['WMKP020'] = 'Meine Termine';
